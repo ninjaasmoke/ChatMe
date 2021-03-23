f960ba2fd86f0c204b4e5c6ce5a757947b378977
Nithin Sai K J <nithins674@gmail.com>
2021-03-23 17:02:45 +0530

    Changed chat ui func

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
                     child: Divider(
                   color: Colors.grey,
                   thickness: .1,
+                  height: 1,
                 )),
                 SizedBox(
                   width: 12,
@@ -48,10 +49,10 @@ class _HomePageState extends State<HomePage> {
             if (index == 0) {
               return Padding(
                 padding: EdgeInsets.only(top: 12),
-                child: getAnimationTile("${randNames[0]}"),
+                child: getChatPage("${randNames[0]}"),
               );
             }
-            return getAnimationTile("${randNames[index % 6]}");
+            return getChatPage("${randNames[index % 6]}");
           },
           itemCount: 20,
         ));
@@ -85,6 +86,45 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       openBuilder: (context, openBuilder) => ChatsPage(name: name),
+    );
+  }
+
+  Widget getChatPage(String name) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ChatsPage(
+                      name: name,
+                    )));
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 52,
+        color: Theme.of(context).scaffoldBackgroundColor,
+        margin: EdgeInsets.symmetric(vertical: 4),
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.grey,
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 8),
+              width: 52,
+              height: 52,
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Text(
+              "$name",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
