c247e823a8f1677b8cffe92ed47c2bdf9acbad96
Nithin Sai K J <nithins674@gmail.com>
2021-03-23 16:37:03 +0530

    light ui theme color change

--- lib/Theme/ThemeDart.dart
+++ lib/Theme/ThemeDart.dart
@@ -12,15 +12,33 @@ PageTransitionsTheme pageTransitionsTheme = PageTransitionsTheme(builders: {
 });
 
 ThemeData lightTheme = ThemeData(
-  primaryColor: Color(0xfff0a010),
-  primarySwatch: Colors.orange,
+  primaryColor: Color(0xff1db954),
+  primarySwatch: Colors.green,
   fontFamily: 'DMSans',
   brightness: Brightness.light,
   backgroundColor: Colors.white,
   pageTransitionsTheme: pageTransitionsTheme,
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+      color: Colors.black,
+    ),
+    bodyText2: TextStyle(color: Colors.grey[400]),
+    subtitle2: TextStyle(color: Colors.grey, fontSize: 12),
+    headline4: TextStyle(
+        color: Colors.grey[900], fontSize: 24, fontWeight: FontWeight.w700),
+    headline6: TextStyle(
+        color: Color(0xff181818), fontSize: 16, fontWeight: FontWeight.w500),
+  ),
   appBarTheme: AppBarTheme(
-    backgroundColor: Color(0xfff0f0f0),
-    textTheme: TextTheme(headline6: TextStyle(color: Color(0xfff0a010))),
+    // backgroundColor: Color(0xfff0f0f0),
+    // backgroundColor: Color(0xfff0a010),
+    backgroundColor: Color(0xfff8f8f8),
+    textTheme: TextTheme(
+        headline6: TextStyle(
+      color: Color(0xff080808),
+      fontSize: 20,
+      fontFamily: 'DMSans',
+    )),
     elevation: 0,
   ),
 );
@@ -40,10 +58,7 @@ ThemeData darkTheme = ThemeData(
       headline4: TextStyle(
           color: Colors.grey[100], fontSize: 24, fontWeight: FontWeight.w700),
       headline6: TextStyle(
-          fontFamily: 'DMSans',
-          color: Color(0xfff0f0f0),
-          fontSize: 16,
-          fontWeight: FontWeight.w500)),
+          color: Color(0xfff0f0f0), fontSize: 16, fontWeight: FontWeight.w500)),
   iconTheme: IconThemeData(color: Colors.white),
   scaffoldBackgroundColor: Color(0xff080808),
   appBarTheme: AppBarTheme(
